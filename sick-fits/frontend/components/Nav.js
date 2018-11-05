7f357c27610a8399e7f3399c8dcd7cd44b2a352b
Monicacholico <monicachol@hotmail.com>
2018-11-04 20:04:56 -0600

    saving

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -4,13 +4,25 @@ import NavStyles from './styles/NavStyles';
 const Nav = () => (
     <NavStyles>
         <Link href="/index">
-            <a>Index</a>
+            <a>Home</a>
         </Link>
-
         <Link href="/sell">
             <a>Sell</a>
         </Link>
+        {/*<Link href ="/items">*/}
+            {/*<a>Items</a>*/}
+        {/*</Link>*/}
+        {/*<Link href ="/signup">*/}
+            {/*<a>SignUp</a>*/}
+        {/*</Link>*/}
+        {/*<Link href ="/orders">*/}
+            {/*<a>Orders</a>*/}
+        {/*</Link>*/}
+        {/*<Link href ="/me">*/}
+            {/*<a>Account</a>*/}
+        {/*</Link>*/}
     </NavStyles>
+
 
 );
 
