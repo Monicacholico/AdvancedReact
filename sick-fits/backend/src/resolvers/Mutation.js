59f6fd5d42d2f79335affc752628d8e6cc456faa
Monicacholico <monicachol@hotmail.com>
2018-11-04 23:22:22 -0600

    saving

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,30 @@
-const mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info){
 
-module.exports = mutations;
+        const item = await ctx.db.mutation.createItem({
+            data:{
+                title: args.title,
+                description: args.description,
+                image: args.image,
+                largeImage: args.largeImage,
+                price: args.price
+            }
+        }, info);
+
+        return item;
+        console.log(item);
+    }
+
+
+
+
+    // createDog(parent, args, ctx, info){
+    //     global.dogs = global.dogs || [];
+    //     //create a Dog
+    //     const newDog = {name: args.name};
+    //     global.dogs.push(newDog);
+    //     return newDog;
+    // },
+};
+
+module.exports = Mutations;
