da5af701b169d255022b06156fc92eb8aae94294
Monicacholico <monicachol@hotmail.com>
2018-11-06 21:37:52 -0600

    saving 16 module 4

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -9,18 +9,18 @@ const Nav = () => (
         <Link href="/sell">
             <a>Sell</a>
         </Link>
-        {/*<Link href ="/items">*/}
-            {/*<a>Items</a>*/}
-        {/*</Link>*/}
-        {/*<Link href ="/signup">*/}
-            {/*<a>SignUp</a>*/}
-        {/*</Link>*/}
-        {/*<Link href ="/orders">*/}
-            {/*<a>Orders</a>*/}
-        {/*</Link>*/}
-        {/*<Link href ="/me">*/}
-            {/*<a>Account</a>*/}
-        {/*</Link>*/}
+        <Link href ="/items">
+            <a>Shop</a>
+        </Link>
+        <Link href ="/signup">
+            <a>SignUp</a>
+        </Link>
+        <Link href ="/orders">
+            <a>Orders</a>
+        </Link>
+        <Link href ="/me">
+            <a>Account</a>
+        </Link>
     </NavStyles>
 
 
---
+++ sick-fits/frontend/pages/items.js
@@ -0,0 +1,6 @@
+import Items from './index';
+
+
+
+
+export default Items;
