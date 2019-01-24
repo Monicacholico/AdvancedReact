fdb65f98540a18acc6605cd26dec5f7f00484217
Monicacholico <monicachol@hotmail.com>
2019-01-23 21:50:22 -0600

    finished video 18

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
-import { Mutation } from 'react-apollo';
+import React, {Component} from 'react';
+import {Mutation} from 'react-apollo';
 import gql from 'graphql-tag';
 import Router from 'next/router';
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
 import Error from './ErrorMessage';
-
 
 
 const CREATE_ITEM_MUTATION = gql`
@@ -41,7 +40,27 @@ class CreateItem extends Component {
         const {name, type, value} = e.target;
         const val = type === 'number' ? parseFloat(value) :
             value;
-        this.setState({ [name]: val });
+        this.setState({[name]: val});
+    };
+
+    uploadFile = async e => {
+        console.log('uploading file...');
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sickfits');
+
+        const res = await fetch
+        ('https://api.cloudinary.com/v1_1/dxtionkli/image/upload', {
+            method: 'POST',
+            body: data
+        });
+        const file = await res.json();
+        console.log(file);
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url,
+        });
     };
 
     render() {
@@ -51,20 +70,33 @@ class CreateItem extends Component {
                     <Form
                         data-test="form"
                         onSubmit={async e => {
-                        e.preventDefault();
-                        const res = await createItem();
-                        console.log(res)
-                        Router.push({
-                            pathname: '/item',
-                            query: {id: res.data.createItem.id},
-                        });
-                    }}
+                            e.preventDefault();
+                            const res = await createItem();
+                            console.log(res)
+                            Router.push({
+                                pathname: '/item',
+                                query: {id: res.data.createItem.id},
+                            });
+                        }}
                     >
                         <Error error={error}/>
-                        <fieldset disabled ={loading} aria-busy={loading}>
+                        <fieldset disabled={loading} aria-busy={loading}>
                             <label htmlFor="title">
-                                Title
+                                Image
                                 <input
+                                    type="file"
+                                    id="file"
+                                    name="file"
+                                    placeholder="Upload an image"
+                                    required
+                                    onChange={this.uploadFile}
+                                />
+                                {this.state.image && <img width="200" src={this.state.image}
+                                                          alt="Upload Preview"/>}
+                                    </label>
+                                    <label htmlFor="title">
+                                    Title
+                                    <input
                                     type="text"
                                     id="title"
                                     name="title"
@@ -72,12 +104,12 @@ class CreateItem extends Component {
                                     required
                                     value={this.state.title}
                                     onChange={this.handleChange}
-                                />
-                            </label>
+                                    />
+                                    </label>
 
-                            <label htmlFor="price">
-                                Price
-                                <input
+                                    <label htmlFor="price">
+                                    Price
+                                    <input
                                     type="number"
                                     id="price"
                                     name="price"
@@ -85,28 +117,28 @@ class CreateItem extends Component {
                                     required
                                     value={this.state.price}
                                     onChange={this.handleChange}
-                                />
-                            </label>
+                                    />
+                                    </label>
 
-                            <label htmlFor="description">
-                                Description
-                                <textarea
+                                    <label htmlFor="description">
+                                    Description
+                                    <textarea
                                     id="description"
                                     name="description"
                                     placeholder="Enter a Description"
                                     required
                                     value={this.state.description}
                                     onChange={this.handleChange}
-                                />
-                            </label>
-                            <button type='submit'>Submit</button>
-                        </fieldset>
-                    </Form>
-                )}
-            </Mutation>
-        );
-    }
-}
+                                    />
+                                    </label>
+                                    <button type='submit'>Submit</button>
+                                    </fieldset>
+                                    </Form>
+                                    )}
+                            </Mutation>
+                            );
+                            }
+                            }
 
-export default CreateItem;
-export { CREATE_ITEM_MUTATION };
+                            export default CreateItem;
+                            export {CREATE_ITEM_MUTATION};
