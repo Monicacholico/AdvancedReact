e8b8c0877fcaf49ed987a87e3423e0e0dd6eb904
Monicacholico <monicachol@hotmail.com>
2019-01-26 23:52:31 -0600

    finished video 20

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -33,9 +33,18 @@ const Mutations = {
             where: {
                 id: args.id,
             },
-        }, info);
+        }, info
+        );
     },
+    async deleteItem(parent, args, ctx, info){
+        const where= {id: args.id};
+        //1.find the item
+        const item = await ctx.db.query.item({ where}, `{id title`);
+        //2. Check if they own thata item, or have the permissions
 
+        //3. Delete it!
+        return ctx.db.mutation.deleteItem({where }, info);
+    }
 };
 
 module.exports = Mutations;
--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -5,26 +5,24 @@ import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
+import DeleteItem from './DeleteItem';
+
 
 
 export default class Item extends Component {
-    static propTypes = {
-        item: PropTypes.object.isRequired,
-    };
-
     render() {
         const {item} = this.props;
         return (<ItemStyles>
                 {item.image &&<img src={item.image} alt={item.title}/>}
-            <Title>
-                <Link href={{
-                    pathname: '/item',
-                    query: {id: item.id},
-                }}>
-                    <a>{item.title}</a>
-                </Link>
-            </Title>
-            <PriceTag>{formatMoney(item.price)}</PriceTag>
+                <Title>
+                    <Link href={{
+                        pathname: '/item',
+                        query: {id: item.id},
+                    }}>
+                        <a>{item.title}</a>
+                    </Link>
+                </Title>
+                <PriceTag>{formatMoney(item.price)}</PriceTag>
                 <p>{item.description}</p>
 
                 <div className="buttonList">
@@ -36,8 +34,14 @@ export default class Item extends Component {
                     >
                         <a>Edit</a>
                     </Link>
+                    <button>Add To Cart</button>
+            <DeleteItem id={item.id}>Delete this Item</DeleteItem>
                 </div>
-                </ItemStyles>
-                );
-            }
+            </ItemStyles>
+    );
+    }
+
+    static propTypes = {
+        item: PropTypes.object.isRequired,
+    };
         }
