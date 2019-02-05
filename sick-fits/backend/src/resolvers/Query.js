b85f81a41c0c092fa57eb21f396f9439cd86f60b
Monicacholico <monicachol@hotmail.com>
2019-02-04 20:41:39 -0600

    pagination working

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -4,10 +4,10 @@ const Query = {
     items: forwardTo('db'),
     item: forwardTo('db'),
     itemsConnection: forwardTo('db'),
-   async items(parent, args, ctx, info){
-        const items= await ctx.db.query.items();
-        return items;
-    }
+   // async items(parent, args, ctx, info){
+   //      const items= await ctx.db.query.items();
+   //      return items;
+   //  }
 };
 
 module.exports = Query;
