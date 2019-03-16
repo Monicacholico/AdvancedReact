f359d8e1985e61c26a0c6ac0231b23fb06e5ab47
Monicacholico <monicachol@hotmail.com>
2019-03-16 00:14:05 -0500

    finished video 34

--- sick-fits/frontend/components/Permissions.js
+++ sick-fits/frontend/components/Permissions.js
@@ -1,7 +1,18 @@
 import {Query} from 'react-apollo';
 import Error from './ErrorMessage';
 import gql from 'graphql-tag';
+import Table from './styles/Table';
+import Suckb from './styles/Suckb';
 
+
+const possiblePermissions = [
+    'ADMIN',
+    'USER',
+    'ITEMCREATE',
+    'ITEMUPDATE',
+    'ITEMDELETE',
+    'PERMISSIONUPDATE'
+];
 
 const ALL_USERS_QUERY = gql`
     query{
@@ -15,14 +26,60 @@ const ALL_USERS_QUERY = gql`
 `;
 
 const Permissions = props => (
-   <Query query={ALL_USERS_QUERY}>
-       {({data, loading, error})=>(
-           <div>
-               <Error error={error}/>
-               <p>hey</p>
-           </div>
-       )}
-   </Query>
+    <Query query={ALL_USERS_QUERY}>
+        {({data, loading, error}) => (
+            <div>
+                <Error error={error}/>
+                <div>
+                    <h2>Manage Permissions</h2>
+                    <Table>
+                        <thead>
+                        <tr>
+                            <th>
+                                Name
+                            </th>
+                            <th>
+                                Email
+                            </th>
+                            {possiblePermissions.map(permission =>
+                                <th>{permission}</th>)}
+                            <th>
+
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {data.users.map(user => user.name)}
+                        </tbody>
+                    </Table>
+                </div>
+                <p>hey</p>
+            </div>
+        )}
+    </Query>
 );
+
+class User extends React.Component {
+    render() {
+        const user = this.props.user;
+        return (
+            <tr>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                {possiblePermissions.map(permission => (
+                    <td>
+                        <label htmlFor={`${user.id}-permission-$
+                       {permission}`}>
+                            <input type="checkbox"/>
+                        </label>
+                    </td>
+                ))}
+                <td>
+                    <SickButton>Update</SickButton>
+                </td>
+            </tr>
+        );
+    }
+}
 
 export default Permissions;
