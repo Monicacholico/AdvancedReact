b3b7fa28088b776853fb7771f3940b489b25900d
Monicacholico <monicachol@hotmail.com>
2019-03-27 23:10:26 -0500

    now really finished video 41

--- sick-fits/frontend/components/AddToCart.js
+++ sick-fits/frontend/components/AddToCart.js
@@ -17,15 +17,21 @@ const ADD_TO_CART_MUTATION = gql`
 class AddToCart extends React.Component {
     render() {
         const {id} = this.props;
-        return <Mutation
-            mutation={ADD_TO_CART_MUTATION}
-            variables={{
-                id,
-            }}
-            refetchQueries={[{query: CURRENT_USER_QUERY}]}
-        >
-            {addToCart => <button onClick={addToCart}>Add To Cart</button>}
-        </Mutation>
+        return (
+            <Mutation
+                mutation={ADD_TO_CART_MUTATION}
+                variables={{
+                    id,
+                }}
+                refetchQueries={[{query: CURRENT_USER_QUERY}]}
+            >
+                {(addToCart, {loading}) => (
+                    <button disabled={loading} onClick={addToCart}>
+                        Add{loading && 'ing'} To Cart
+                    </button>
+                )}
+            </Mutation>
+        );
     }
 }
 
