8a78173cf6efd9a53b5f6975b5e081432b845693
Monicacholico <monicachol@hotmail.com>
2019-03-31 00:09:59 -0500

    finished video 44

--- sick-fits/frontend/components/CartCount.js
+++ sick-fits/frontend/components/CartCount.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { TransitionGroup, CSSTransition} from 'react-transition-group';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
+
+const AnimationStyles = styled.span`
+    position: relative;
+    .count {
+        display: block;
+        position: relative;
+        transition: all 0.4s;
+        backface-visibility: hidden;
+    }
+    .count-enter {
+        transform: scale(4) rotateX(0.5turn);
+    }
+    .count-enter-active {
+        transform: rotateX(0);
+    }   
+    .count-exit {
+        top: 0;
+        position: absolute;
+        transform: scale(4) rotateX(0.5turn);
+    }
+`;
 
 const Dot = styled.div`
     background: ${props => props.theme.red};
@@ -11,15 +32,25 @@ const Dot = styled.div`
     padding: 0.5rem;
     line-height: 2rem;
     min-width: 3rem;
-    margin-left: 1 rem;
+    margin-left: 1rem;
     font-weight: 100;
     font-feature-settings: 'tnum';
     font-variant-numeric: tabular-nums;
 `;
 
 const CartCount = ({count}) => (
-    <Dot>{count}</Dot>
+    <AnimationStyles>
+        <TransitionGroup>
+            <CSSTransition
+                className="count"
+                classNames="count"
+                key={count}
+                timeout={{enter: 400, exit: 400}}>
+                <Dot>{count}</Dot>
+            </CSSTransition>
+        </TransitionGroup>
+    </AnimationStyles>
 
-)
+);
 
 export default CartCount;
