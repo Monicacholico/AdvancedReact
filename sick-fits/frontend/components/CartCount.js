a2b11c373faa5389a01f259fddf127318383e1f6
Monicacholico <monicachol@hotmail.com>
2019-03-30 23:51:44 -0500

    working in video 44

---
+++ sick-fits/frontend/components/CartCount.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { TransitionGroup, CSSTransition} from 'react-transition-group';
+
+
+const Dot = styled.div`
+    background: ${props => props.theme.red};
+    color: white;
+    border-radius: 50%;
+    padding: 0.5rem;
+    line-height: 2rem;
+    min-width: 3rem;
+    margin-left: 1 rem;
+    font-weight: 100;
+    font-feature-settings: 'tnum';
+    font-variant-numeric: tabular-nums;
+`;
+
+const CartCount = ({count}) => (
+    <Dot>{count}</Dot>
+
+)
+
+export default CartCount;
