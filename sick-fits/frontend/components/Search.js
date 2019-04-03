b234dfa0016e9d22d2ffa818f4cc25b8e588657f
Monicacholico <monicachol@hotmail.com>
2019-04-02 23:50:36 -0500

    finished video 47

--- sick-fits/frontend/components/Search.js
+++ sick-fits/frontend/components/Search.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Downshift from 'downshift';
 import Router from 'next/router';
-import {ApolloConsumer} from 'react-adopt';
+import {ApolloConsumer} from 'react-apollo';
 import gql from 'graphql-tag';
 import debounce from 'lodash.debounce';
 import { DropDown, DropDownItem, SearchStyles} from './styles/DropDown';
 
 const SEARCH_ITEMS_QUERY = gql`
     query SEARCH_ITEMS_QUERY($searchTerm: String!){
-        item(where: {
+        items(where: {
             OR: [{ title_contains: $searchTerm},
                 {description_contains: $searchTerm}] }){
             id
@@ -20,16 +20,44 @@ const SEARCH_ITEMS_QUERY = gql`
 
 
 class AutoComplete extends React.Component {
+    state = {
+        items: [],
+        loading: false,
+    };
+    onChange = debounce(async (e, client) => {
+        //turn loading on
+        this.setState({loading: true});
+        const res = await client.query({
+            query: SEARCH_ITEMS_QUERY,
+            variables: {searchTerm: e.target.value},
+        });
+        this.setState({
+            items: res.data.items,
+            loading: false,
+        });
+    }, 350);
     render(){
         return(
         <SearchStyles>
             <div>
                 <ApolloConsumer>
-
+                    {client => (
+                        <input
+                            type="search"
+                            onChange ={e =>{
+                        e.persist();
+                        this.onChange(e, client);
+                        }}
+                        />
+                        )}
                 </ApolloConsumer>
-                <input type="search"/>
                 <DropDown>
-                    <p>Items will go here</p>
+                    {this.state.items.map(item =>
+                        <DropDownItem key={item.id}>
+                        <img width="50" src={item.image} alt=
+                            {item.title}/>
+                        {item.title}
+                    </DropDownItem>)}
                 </DropDown>
             </div>
         </SearchStyles>
